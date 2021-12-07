e4bd97a50665ebd54ce5cd14f27f4c5e40d8bd6b
genie-oh <genie-oh@outlook.com>
2021-12-07 22:12:05 +0900

    mod: apply HomeContext > pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,17 @@
+import HomeContext from "../components/Context/HomeContext";
+
 export default function Home() {
     return (
-        <div className="bg-blue-100">
-            <p>
-                Hello!
-                <br />
-                こんにちは!
-                <br />
-                안녕하세요!
-            </p>
-            <p className="text-2xl font-bold">
-                Hello!
-                <br />
-                こんにちは!
-                <br />
-                안녕하세요!
-            </p>
-            <p className="text-5xl font-bold text-">
-                Hello!
-                <br />
-                こんにちは!
-                <br />
-                안녕하세요!
-            </p>
-        </div>
+        <HomeContext.Provider value={{}}>
+            <div className="bg-blue-100">
+                <p>
+                    Hello!
+                    <br />
+                    こんにちは!
+                    <br />
+                    안녕하세요!
+                </p>
+            </div>
+        </HomeContext.Provider>
     );
 }
