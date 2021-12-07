148bc0b1e5a780f5913f859bd62204d54f6a075e
genie-oh <genie-oh@outlook.com>
2021-12-07 22:00:57 +0900

    mod: test tailwind > index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
 export default function Home() {
-    return <div>hello</div>;
+    return (
+        <div className="bg-blue-100">
+            <p>
+                Hello!
+                <br />
+                こんにちは!
+                <br />
+                안녕하세요!
+            </p>
+            <p className="text-2xl font-bold">
+                Hello!
+                <br />
+                こんにちは!
+                <br />
+                안녕하세요!
+            </p>
+            <p className="text-5xl font-bold text-">
+                Hello!
+                <br />
+                こんにちは!
+                <br />
+                안녕하세요!
+            </p>
+        </div>
+    );
 }
