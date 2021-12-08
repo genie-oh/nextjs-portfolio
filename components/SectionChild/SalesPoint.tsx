6c0d9948ece31abc6ad8e48a3c251f37b0e3dd76
genie-oh <genie-oh@outlook.com>
2021-12-08 17:21:05 +0900

    mod: refer to dataMySalesPoint data > components/SectionChild/SalesPoint.tsx

--- components/SectionChild/SalesPoint.tsx
+++ components/SectionChild/SalesPoint.tsx
@@ -1,20 +1,22 @@
 import ImageTextCard from "../Parts/ImageTextCard";
 
+import i18nData from "../../i18n/i18nData";
+import { dataMySalesPoint } from "../../datas/dataMySalesPoint";
+
 const SalesPoint = () => {
     return (
         <div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-                {Array(4)
-                    .fill(0)
-                    .map((v, idx) => {
-                        return (
-                            <ImageTextCard
-                                imgUrl="/img/sales_point/architect.svg"
-                                title={"title" + idx}
-                                text={"text" + idx}
-                            />
-                        );
-                    })}
+                {dataMySalesPoint.map((item, idx) => {
+                    return (
+                        <ImageTextCard
+                            key={idx}
+                            imgUrl={item.imgUrl}
+                            title={item.title}
+                            text={i18nData(item.text_i18n)}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
