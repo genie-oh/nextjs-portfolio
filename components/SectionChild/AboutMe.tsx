062ec433790be0b4a21da1a1d60565875ff76e40
genie-oh <genie-oh@outlook.com>
2021-12-09 20:20:30 +0900

    mod: prototype Layout of Aboutme > SectionChild/AboutMe.tsx

--- components/SectionChild/AboutMe.tsx
+++ components/SectionChild/AboutMe.tsx
@@ -1,5 +1,15 @@
 const AboutMe = () => {
-    return <div>AboutMe!</div>;
+    return (
+        <div className="my-20">
+            <div className="grid grid-cols-1 pt-10 lg:grid-cols-2">
+                <div className="m-5 bg-gray-100 h-96">profile</div>
+                <div className="m-5 bg-gray-100 h-96">strength34</div>
+            </div>
+
+            <div className="m-5 bg-gray-100 h-96">engineering skills</div>
+            <div className="m-5 bg-gray-100 h-96">development skills</div>
+        </div>
+    );
 };
 
 export default AboutMe;
