c6aef4c1bcd511b852dbbbb0c3a7c221ce58eb87
genie-oh <genie-oh@outlook.com>
2021-12-09 21:27:32 +0900

    mod: refer AboutMeStrength component

--- components/SectionChild/AboutMe.tsx
+++ components/SectionChild/AboutMe.tsx
@@ -1,11 +1,12 @@
 import AboutMeProfile from "../SectionChildParts/AboutMeProfile";
+import AboutMeStrength from "../SectionChildParts/AboutMeStrength";
 
 const AboutMe = () => {
     return (
         <div className="my-20">
             <div className="grid grid-cols-1 pt-10 lg:grid-cols-2">
                 <AboutMeProfile />
-                <div className="m-5 bg-gray-100 h-96">strength34</div>
+                <AboutMeStrength />
             </div>
 
             <div className="m-5 bg-gray-100 h-96">engineering skills</div>
