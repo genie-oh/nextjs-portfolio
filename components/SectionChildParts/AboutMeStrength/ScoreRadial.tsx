0f4effc646d5258f53112f0f9a67b5b0e4472fb7
genie-oh <genie-oh@outlook.com>
2021-12-09 23:18:41 +0900

    new: components/SectionChildParts/AboutMeStrength/ScoreRadial.tsx

---
+++ components/SectionChildParts/AboutMeStrength/ScoreRadial.tsx
@@ -0,0 +1,9 @@
+const ScoreRadial = () => {
+    return (
+        <div className="w-full -mb-10" style={{ height: "350px" }}>
+            <div className="text-center text-red-500">radial graph</div>
+        </div>
+    );
+};
+
+export default ScoreRadial;
