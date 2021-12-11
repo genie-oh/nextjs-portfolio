da17791641277c8800040a7c368c8b59f125e791
genie-oh <genie-oh@outlook.com>
2021-12-11 16:05:08 +0900

    mod: impl send message to googlesheet > components/SectionChild/ContactMe.tsx

--- components/SectionChild/ContactMe.tsx
+++ components/SectionChild/ContactMe.tsx
@@ -3,13 +3,50 @@ import { useState } from "react";
 import i18nData from "../../i18n/i18nData";
 import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
+import dateFormat from "dateformat";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+
 const ContactMe = () => {
+    //-- send message to google sheet
+    const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
+    const SHEET_ID = process.env.REACT_APP_SHEET_ID;
+    const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
+    const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
+
+    const googleSheetService = new GoogleSpreadsheet(SPREADSHEET_ID);
+
+    const sendToGoogleSheetAsAppendRow = async (rowData) => {
+        try {
+            await googleSheetService.useServiceAccountAuth({
+                client_email: CLIENT_EMAIL,
+                private_key: PRIVATE_KEY,
+            });
+            await googleSheetService.loadInfo();
+
+            const sheet = googleSheetService.sheetsById[SHEET_ID];
+            await sheet.addRow(rowData);
+        } catch (e) {
+            alert("sorry: message sending error occured");
+        }
+    };
+
+    //-- submit Event Handle
     const [formSubmitted, setFormSubmitted] = useState(false);
 
     const handleSubmit = (ev) => {
         ev.preventDefault();
 
-        //@todo form submit logic
+        const rowData = {
+            NAME: ev.target.elements.name.value,
+            EMAIL: ev.target.elements.email.value,
+            MESSAGE: ev.target.elements.message.value,
+            DATE: dateFormat(new Date(), "yyyy-mm-dd hh:mm:ss"),
+        };
+
+        //throwable
+        sendToGoogleSheetAsAppendRow(rowData);
+
+        //if success
         setFormSubmitted(true);
     };
 
