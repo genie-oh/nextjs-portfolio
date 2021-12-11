0023b6e1160c952b846ff3822935c22db36c8191
genie-oh <genie-oh@outlook.com>
2021-12-11 16:03:00 +0900

    mod: add webpack config for fixing error > next.config.js
    - error
    Module not found: Can't resolve 'child_process'

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
     locales: ["en", "ja", "ko"],
     defaultLocale: "ja"
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) config.resolve.fallback.fs = false;
+    if (!isServer) config.resolve.fallback.child_process = false;
+    if (!isServer) config.resolve.fallback.net = false;
+    if (!isServer) config.resolve.fallback.dns = false;
+    if (!isServer) config.resolve.fallback.tls = false;
+    return config;
+  },
 }
