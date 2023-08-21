70af5e07f42d36a412aba4c89a885548e4d46dc0
genie_oh <genie_oh@outlook.com>
2023-08-21 17:37:40 +0900

    format: prettier .

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ["en", "ja", "ko"],
-    defaultLocale: "ja"
+    defaultLocale: "ja",
   },
   webpack: (config, { isServer }) => {
     if (!isServer) config.resolve.fallback.fs = false;
@@ -13,9 +13,10 @@ module.exports = {
     return config;
   },
   env: {
-    REACT_APP_SPREADSHEET_ID : process.env.REACT_APP_SPREADSHEET_ID,
+    REACT_APP_SPREADSHEET_ID: process.env.REACT_APP_SPREADSHEET_ID,
     REACT_APP_SHEET_ID: process.env.REACT_APP_SHEET_ID,
     REACT_APP_GOOGLE_CLIENT_EMAIL: process.env.REACT_APP_GOOGLE_CLIENT_EMAIL,
-    REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY: process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY,
+    REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY:
+      process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY,
   },
-}
+};
